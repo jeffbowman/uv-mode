a5f31265cd23b6e80503692cd7280f3ca4fddf2a
Jeff Bowman <jeff.t.bowman@gmail.com>
2025-11-09 15:08:33 -0600

    Drop global flag, empty keymap
    Removed `global` flag on minor mode
    Created empty keymap

--- uv-mode.el
+++ uv-mode.el
@@ -90,7 +90,6 @@ This looks for a .venv directory in the project root."
 (define-minor-mode uv-mode
   "Minor mode for uv virtualenv interaction.
 \\{uv-mode-map}"
-  :global t
   :lighter ""
   :keymap uv-mode-map
   (if uv-mode
